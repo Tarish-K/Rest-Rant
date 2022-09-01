2a93c092545ce2b2ce88115999dcb19669956791
Tarish K <102713730+Tarish-K@users.noreply.github.com>
2022-08-31 22:13:31 -0400

    saved

--- views/default.jsx
+++ views/default.jsx
@@ -7,10 +7,8 @@ function Def (html) {
                 <title>RestRant Live Demo</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/css/style.css"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest"></link>
+                <link rel="icon" type="image/jpg" sizes="16x16" href="/gg.jpg" />
+                
             </head>
             <body>
                 <nav>
